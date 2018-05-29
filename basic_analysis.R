d72facac9aa3ff54a55ab2454538b8dd81b899ef
iriszs <i.m.gorter@st.hanze.nl>
2018-05-29 16:15:14 +0200

    added argument processing, uniform SCE object creation and quality control and filtering

--- basic_analysis.R
+++ basic_analysis.R
@@ -1,62 +1,186 @@
-### combination script with Seurat, scater and CellrangerRkit###
-#Scater for determination of clusters? see heimberg website
-
+# Only for computer at the office
 .libPaths(.libPaths()[2:4])
+# Does adding not work? -> open disk first.
 .libPaths("/media/imgorter/firstDisk/Iris/R")
 .libPaths()
 
 #source("http://cf.10xgenomics.com/supp/cell-exp/rkit-install-2.0.0.R")
 #source("https://bioconductor.org/biocLite.R")
+
+# Libraries
+library(SingleCellExperiment)
+library(scater)
 library(cellrangerRkit)
 library(Seurat)
 library(dplyr)
 
+
+#################################################################
+#                   Argument Processing                         #
+#################################################################
+
+
+# Get the commandline arguments
 args = commandArgs(trailingOnly=TRUE)
 
+# First argument is the output path
 output_path <- args[1]
 
+# Second argument is the run ID
 id <- args[2]
 
+# Third argument is the species (mouse or human)
 species <- args[3]
 
+# split the outputpath on /
+splitted_output_path <- unlist(strsplit(output_path, "/"))
+# Remove the data map and the species map from the path (which are the last two)
+new_output_path <- splitted_output_path[0:(length(splitted_output_path)-2)]
+# Put the arguments back together
+new_output_path <- trimws(paste(new_output_path, collapse = '/'))
+# Create a new R analysis directory where the pdf's will be saved.
+dir.create(paste0(new_output_path, "/R_analysis/", sep=""), showWarnings = FALSE)
 
-if (length(args)==0) {
-  stop("At least one argument must be supplied!", call.=FALSE)
-} else if (length(args)==1) {
-  # default id
-  args[2] = "Study ID unknown"
+# Quit when there are less than 3 arguments
+if (length(args) < 3) {
+  stop("Three arguments must be supplied!", call. = FALSE)
 }
 
-data.data <- Read10X(args[1]) 
 
-data <- CreateSeuratObject(raw.data = data.data, min.cells = 3, min.genes = 100, project = args[2])
+#################################################################
+#                       Create SCE object                       #
+#################################################################
 
-#if data is human, ^MT-, if data is mouse ^mt-
-#maybe figure that out with HG path and MM path
-#what to do when cellranger is run with both
 
+# Cell barcodes output file from Cell Ranger
+cellbarcodes <- read.table(paste0(output_path, "barcodes.tsv"))
+# Gene file output from Cell Ranger
+genenames <- read.table(paste0(output_path, "genes.tsv"))
+# Matrix file output from Cell Ranger
+molecules <- Matrix::readMM(paste0(output_path, "matrix.mtx"))
+
+# set the rownames of the molecules matrix
+rownames(molecules) <- genenames[,1]
+# Set the colnames of the molecules matrix
+colnames(molecules) <- paste(id, cellbarcodes[,1], sep="_")
+
+#Convert molecules as a matrix 
+molecules <- as.matrix(molecules)
+
+# Create a Single Cell Experiment object using the molecules matrix
+sceset <- SingleCellExperiment(assays = list(counts=as.matrix(molecules)))
+
+
+#################################################################
+#                 Quality Control & Filtering                   #
+#################################################################
+
+
+# Find genes that are not expressed in any cell
+keep_feature <- rowSums(counts(sceset) > 0) > 0
+
+# not expressed cells: all cells minus the cells that are above 0
+not_expressed_cells  <- length(keep_feature) - length(which(keep_feature))
+
+# Notify user how many genes will be removed with this filtering step
+print(paste(not_expressed_cells, "genes that are not expressed in any cell have been removed"))
+
+# Remove genes that are not expressed in any cell
+sceset <- sceset[keep_feature, ]
+
+# Open up a PDF to save all QC plots
+pdf(paste0(new_output_path, "/R_analysis/QC.pdf"))
+
+containsSpike <- spikeNames(sceset)
+print(containsSpike)
+
+if (is.null(containsSpike)) {
+  print("Your dataset does not contain any spikes. Skipping spike-ins quality check........")
+  
+} else{
+  isSpike(sceset, "ERCC") <- grepl("^ERCC-", rownames(sceset))
+  plotPhenoData(sceset, aes_string(x = "total_features", y = "pct_counts_ERCC"))
+  # Calculate the quality metrics
+  sceset <- calculateQCMetrics(sceset, feature_controls = list(ERCC = isSpike(sceset, "ERCC")))
+  
+  # ERCC filter might not be possible, it has to be defined by eye? Our data does not have metadata.
+  #filter_by_ERCC <- sceset$batch != "NA19098.r2" & umi$pct_counts_ERCC < 10
+  #table(filter_by_ERCC)
+}
+
+
+# If the species is human, find the human mitochondrial genes
 if (species == "Human") {
-  mito.genes <- grep(pattern = "^MT-", x = rownames(x = data@data), value = TRUE)
+  isSpike(sceset, "MT") <- rownames(sceset) %in% 
+    c("ENSG00000198899", "ENSG00000198727", "ENSG00000198888",
+      "ENSG00000198886", "ENSG00000212907", "ENSG00000198786",
+      "ENSG00000198695", "ENSG00000198712", "ENSG00000198804",
+      "ENSG00000198763", "ENSG00000228253", "ENSG00000198938",
+      "ENSG00000198840")
+  # Calculate QC for human mitochondrial genes
+  sceset <- calculateQCMetrics(sceset, feature_controls = list(MT = isSpike(sceset, "MT")))
+  
+  
+# If the species is mouse, find the mouse mitochondrial genes (TODO: implement)
+} else if (species == "Mouse") {
+  isSpike(sceset, "mt") <- rownames(sceset) %in%
+    c("ENSMUSG")
+  # Calculate QC for mouse mitochondrial genes
+  sceset <- calculateQCMetrics(sceset, feature_controls = list(MT = isSpike(sceset, "mt")))
+  
+
+# If there is a species that is not mouse or human, give a notification to the user.
 } else {
-  mito.genes <- grep(pattern = "^mt-", x = rownames(x = data@data), value = TRUE)
-}  
-
-percent.mito <- Matrix::colSums(data@raw.data[mito.genes, ])/Matrix::colSums(data@raw.data)
-
-data <- AddMetaData(object = data, metadata = percent.mito, col.name = "percent.mito")
-VlnPlot(object = data, features.plot = c("nGene", "nUMI", "percent.mito"), nCol = 3)
-
-par(mfrow = c(1, 2))
-GenePlot(object = data, gene1 = "nUMI", gene2 = "percent.mito")
-GenePlot(object = data, gene1 = "nUMI", gene2 = "nGene")
-
-data <- FilterCells(object = data, subset.names = c("nGene", "percent.mito"), low.thresholds = c(200, -Inf), high.thresholds = c(2500, 0.05))
-
-percentage_removed_cells <- (length(data@data) / length(data@raw.data)) * 100
-
-if (percentage_removed_cells >= 70) {
-  print("Removed over 70% of your cells!")
-} else {
-  print(paste0("Removed ", percentage_removed_cells, "% of your cells"))
+  print("This species is not supported (yet) to find mitochondrial genes")
 }
 
+# Calculate the QC for the mitocho
+
+# Plot ratio between ERCC spike-ins and endogenous RNA
+# Is a main title possible? would be: Percentage of counts in MT genes
+plotPhenoData(sceset, aes_string(x = "total_features", y = "pct_counts_MT"))
+
+# Plot histogram of the library size
+hist(sceset$total_counts, breaks = 100, main = paste("Histogram of library sizes for all cells"))
+# TODO: at 25%?, place the abline, or always 2500?
+abline(v = 2500, col = "red")
+
+# MT filter based on? 
+#filter_by_MT <- sceset$pct_counts_MT < 10
+#table(filter_by_MT)
+
+# Define the filter to keep cells that have more than 2500 counts in a cell
+filter_by_total_counts <- (sceset$total_counts > 2500)
+table(filter_by_total_counts)
+
+# Plot histogram of the detected genes
+hist(sceset$total_features, breaks = 100, main = paste("Histogram of the number of unique detected genes in all cells"))
+# TODO: at 75%? place the abline or always 1500?
+abline(v = 1500, col = "red")
+
+# Define the filter to remove cells that have over 7000 genes in a cell
+filter_by_expr_features <- (sceset$total_features > 7000)
+table(filter_by_expr_features)
+
+# Cell filter based on the previous quality controls.
+# TODO: implement different sceset$use based on spike ins or not
+#sceset$use <- (filter_by_expr_features & filter_by_total_counts & filter_by_ERCC & filter_by_MT)
+sceset$use <- (filter_by_expr_features & filter_by_total_counts)
+
+plotQC(sceset, type = "highest-expression")
+
+#close the PDF
+dev.off()
+
+#filter the genes that are 
+filter_genes <- apply(counts(sceset[ , colData(sceset)$sceset]), 1, function(x) length(x[x > 1]) >= 2)
+rowData(sceset)$use <- filter_genes
+
+table(filter_genes)
+
+# Decide here to filter or not to filter
+
+
+#Confounding factors
+#Normalization
+#Biologic analysis
--- pipeline.py
+++ pipeline.py
@@ -6,14 +6,18 @@ import subprocess
 
 inputdirectory = "/media/imgorter/BD_1T/Iris/Data/BE_10X_pilot_II/"
 #if run_id is empty, use the name of the folder where the data is in
-run_id = "10X_pilot_2"
-output = "/media/imgorter/BD_1T/Iris/Human_Sample/outs/raw_gene_bc_matrices/GRCh38/"
-species = "Human"
+#run_id = "10X_pilot_2"
+#output = "/media/imgorter/BD_1T/Iris/Human_Sample/outs/filtered_gene_bc_matrices/GRCh38/"
+#species = "Human"
+
+run_id = "10X_pilot_1"
+output = "/media/imgorter/BD_1T/Iris/Mouse_Sample/outs/filtered_gene_bc_matrices/mm10/"
+species = "Mouse"
 
 
 
 def perform_Analysis(output, run_id):
-	subprocess.call(["Rscript", "--vanilla", "/media/imgorter/BD_1T/Iris/Scripts/basic_analysis.R", output, run_id, species])
+	subprocess.call(["Rscript", "--vanilla", "/media/imgorter/BD_1T/Iris/Scripts/Pipeline/basic_analysis.R", output, run_id, species])
 	
 	
 

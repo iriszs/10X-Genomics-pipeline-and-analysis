542ef4378fb0d203399d98c76ffa2705505752e7
Iris <i.m.gorter@umcg.nl>
2018-07-03 13:53:42 +0200

    Added tests to prevent unnecessary installation of packages and added two normalization methods (CPM and SCRAN)

--- basic_analysis.R
+++ basic_analysis.R
@@ -1,18 +1,51 @@
 # Only for computer at the office
-.libPaths(.libPaths()[2:4])
+#.libPaths(.libPaths()[2:4])
 # Does adding not work? -> open disk first.
-.libPaths("/media/imgorter/firstDisk/Iris/R")
-.libPaths()
+#.libPaths("/media/imgorter/firstDisk/Iris/R")
+#.libPaths()
 
 #source("http://cf.10xgenomics.com/supp/cell-exp/rkit-install-2.0.0.R")
-#source("https://bioconductor.org/biocLite.R")
+source("https://bioconductor.org/biocLite.R")
+
+
+# Functions to test if package is already installed or not.
+
+# Package test for the packages that need to be installed using install.packages()
+pkgTest <- function(x){
+  if (!require(x,character.only = TRUE)){
+    install.packages(x, repos = "http://cran.us.r-project.org")
+    if(!require(x,character.only = TRUE)) stop("Package not found")
+  }
+}
+
+# Package test for the packages that need to be installed using bioconductor
+bioPkgTest <- function(x){
+  if (!require(x,character.only = TRUE)){
+    source("https://bioconductor.org/biocLite.R")
+    biocLite(x)
+    if(!require(x,character.only = TRUE)) stop("Package not found")
+  }
+}
+
 
 # Libraries
-library(SingleCellExperiment)
-library(scater)
-library(cellrangerRkit)
-library(Seurat)
-library(dplyr)
+
+pkgTest("devtools")
+
+if (!require(scRNA.seq.funcs)){
+  devtools::install_github("hemberg-lab/scRNA.seq.funcs")
+}
+
+bioPkgTest("SingleCellExperiment")
+bioPkgTest("scater")
+bioPkgTest("scran")
+bioPkgTest("limma")
+
+# Options
+
+options(stringsAsFactors = FALSE)
+set.seed(1234567)
+
 
 
 #################################################################
@@ -32,6 +65,7 @@ id <- args[2]
 # Third argument is the species (mouse or human)
 species <- args[3]
 
+anno <- args[4]
 # split the outputpath on /
 splitted_output_path <- unlist(strsplit(output_path, "/"))
 # Remove the data map and the species map from the path (which are the last two)
@@ -42,15 +76,14 @@ new_output_path <- trimws(paste(new_output_path, collapse = '/'))
 dir.create(paste0(new_output_path, "/R_analysis/", sep=""), showWarnings = FALSE)
 
 # Quit when there are less than 3 arguments
-if (length(args) < 3) {
-  stop("Three arguments must be supplied!", call. = FALSE)
+if (length(args) < 4) {
+  stop("Four arguments must be supplied!", call. = FALSE)
 }
 
 
 #################################################################
 #                       Create SCE object                       #
 #################################################################
-
 
 # Cell barcodes output file from Cell Ranger
 cellbarcodes <- read.table(paste0(output_path, "barcodes.tsv"))
@@ -60,16 +93,26 @@ genenames <- read.table(paste0(output_path, "genes.tsv"))
 molecules <- Matrix::readMM(paste0(output_path, "matrix.mtx"))
 
 # set the rownames of the molecules matrix
-rownames(molecules) <- genenames[,1]
+rownames(molecules) <- make.names(genenames[,2], unique = TRUE)
 # Set the colnames of the molecules matrix
 colnames(molecules) <- paste(id, cellbarcodes[,1], sep="_")
 
 #Convert molecules as a matrix 
 molecules <- as.matrix(molecules)
 
-# Create a Single Cell Experiment object using the molecules matrix
-sceset <- SingleCellExperiment(assays = list(counts=as.matrix(molecules)))
-
+# If annotation file is not present.
+if (anno == ""){
+  print("No annotation file.")
+  # Create a Single Cell Experiment object using the molecules matrix
+  sceset <- SingleCellExperiment(assays = list(counts=as.matrix(molecules)))
+} else{
+  # If annotation file is present
+  annotations <- read.table(paste(output_path, anno, sep= ""), sep = "\t", header = TRUE)
+  # Create a Single Cell Experiment object using the molecules matrix
+  sceset <- SingleCellExperiment(assays = list(counts = as.matrix(molecules)), colData = annotations)
+  
+  
+}
 
 #################################################################
 #                 Quality Control & Filtering                   #
@@ -92,95 +135,111 @@ sceset <- sceset[keep_feature, ]
 pdf(paste0(new_output_path, "/R_analysis/QC.pdf"))
 
 containsSpike <- spikeNames(sceset)
-print(containsSpike)
+
 
 if (is.null(containsSpike)) {
   print("Your dataset does not contain any spikes. Skipping spike-ins quality check........")
   
 } else{
   isSpike(sceset, "ERCC") <- grepl("^ERCC-", rownames(sceset))
-  plotPhenoData(sceset, aes_string(x = "total_features", y = "pct_counts_ERCC"))
+  plotPhenoData(sceset, aes_string(x = "total_features", y = "pct_counts_ERCC")) + ggtitle("ERCC Spike-in counts")
   # Calculate the quality metrics
   sceset <- calculateQCMetrics(sceset, feature_controls = list(ERCC = isSpike(sceset, "ERCC")))
   
-  # ERCC filter might not be possible, it has to be defined by eye? Our data does not have metadata.
-  #filter_by_ERCC <- sceset$batch != "NA19098.r2" & umi$pct_counts_ERCC < 10
-  #table(filter_by_ERCC)
 }
 
 
 # If the species is human, find the human mitochondrial genes
 if (species == "Human") {
-  isSpike(sceset, "MT") <- rownames(sceset) %in% 
-    c("ENSG00000198899", "ENSG00000198727", "ENSG00000198888",
-      "ENSG00000198886", "ENSG00000212907", "ENSG00000198786",
-      "ENSG00000198695", "ENSG00000198712", "ENSG00000198804",
-      "ENSG00000198763", "ENSG00000228253", "ENSG00000198938",
-      "ENSG00000198840")
+  isSpike(sceset, "MT") <- grep(pattern = "^MT\\.", rownames(sceset), value = TRUE)
   # Calculate QC for human mitochondrial genes
   sceset <- calculateQCMetrics(sceset, feature_controls = list(MT = isSpike(sceset, "MT")))
   
-  
-# If the species is mouse, find the mouse mitochondrial genes (TODO: implement)
+  # If the species is mouse, find the mouse mitochondrial genes
 } else if (species == "Mouse") {
-  isSpike(sceset, "mt") <- rownames(sceset) %in%
-    c("ENSMUSG")
+  isSpike(sceset, "mt") <- grep(pattern = "^mt\\.", rownames(sceset), value = TRUE)
   # Calculate QC for mouse mitochondrial genes
   sceset <- calculateQCMetrics(sceset, feature_controls = list(MT = isSpike(sceset, "mt")))
   
-
-# If there is a species that is not mouse or human, give a notification to the user.
+  # If there is a species that is not mouse or human, give a notification to the user.
 } else {
   print("This species is not supported (yet) to find mitochondrial genes")
 }
 
-# Calculate the QC for the mitocho
-
 # Plot ratio between ERCC spike-ins and endogenous RNA
 # Is a main title possible? would be: Percentage of counts in MT genes
-plotPhenoData(sceset, aes_string(x = "total_features", y = "pct_counts_MT"))
+plotPhenoData(sceset, aes_string(x = "total_features", y = "pct_counts_MT")) + ggtitle("Percentage of counts in MT genes")
 
 # Plot histogram of the library size
 hist(sceset$total_counts, breaks = 100, main = paste("Histogram of library sizes for all cells"))
-# TODO: at 25%?, place the abline, or always 2500?
-abline(v = 2500, col = "red")
-
-# MT filter based on? 
-#filter_by_MT <- sceset$pct_counts_MT < 10
-#table(filter_by_MT)
-
-# Define the filter to keep cells that have more than 2500 counts in a cell
-filter_by_total_counts <- (sceset$total_counts > 2500)
-table(filter_by_total_counts)
 
 # Plot histogram of the detected genes
 hist(sceset$total_features, breaks = 100, main = paste("Histogram of the number of unique detected genes in all cells"))
-# TODO: at 75%? place the abline or always 1500?
-abline(v = 1500, col = "red")
 
-# Define the filter to remove cells that have over 7000 genes in a cell
+# Filters
+filter_by_total_counts <- (sceset$total_counts > 25000)
+table(filter_by_total_counts)
+
 filter_by_expr_features <- (sceset$total_features > 7000)
 table(filter_by_expr_features)
 
-# Cell filter based on the previous quality controls.
-# TODO: implement different sceset$use based on spike ins or not
-#sceset$use <- (filter_by_expr_features & filter_by_total_counts & filter_by_ERCC & filter_by_MT)
+# 
 sceset$use <- (filter_by_expr_features & filter_by_total_counts)
 
-plotQC(sceset, type = "highest-expression")
+# Plot the genes that have the highest expression
+plotQC(sceset, type = "highest-expression") + ggtitle("Genes with the highest expression")
 
-#close the PDF
+# Close the PDF
 dev.off()
 
-#filter the genes that are 
-filter_genes <- apply(counts(sceset[ , colData(sceset)$sceset]), 1, function(x) length(x[x > 1]) >= 2)
+
+#################################################################
+#                         Normalization                         #
+#################################################################
+
+# Remove genes that are 'undetectable'
+filter_genes <- apply(counts(sceset[ , colData(sceset)$use]), 1, function(x) length(x[x > 1]) >= 2)
 rowData(sceset)$use <- filter_genes
 
 table(filter_genes)
+dim(sceset[rowData(sceset)$use, colData(sceset)$use])
 
-# Decide here to filter or not to filter
+# Lognormalize the raw counts
+assay(sceset, "logcounts_raw") <- log2(counts(sceset) + 1)
+reducedDim(sceset) <- NULL
+
+# Only use the data that matches the quality control requirements
+sceset.qc <- sceset[rowData(sceset)$use, colData(sceset)$use]
+endog_genes <- !rowData(sceset.qc)$is_feature_control
+
+# Open PDF for the normalization comparison plots
+pdf(paste0(new_output_path, "/R_analysis/Normalization.pdf"))
 
 
-#Confounding factors
-#Normalization
-#Biologic analysis
+# CPM normalization
+
+
+assay(sceset, "logcounts_raw") <- log2(counts(sceset) + 1)
+
+logcounts(sceset) <- log2(calculateCPM(sceset, use.size.factors = FALSE) + 1)
+
+# Compare raw (log normalized) with CPM using a relative log expression plot
+
+plotRLE(sceset[endog_genes, ], exprs_mats = list(Raw = sceset@assays$data$logcounts_raw, CPM = sceset@assays$data$logcounts), exprs_logged = c(TRUE, TRUE))
+
+
+# SCRAN normalization
+
+
+qclust <- quickCluster(sceset.qc)
+sceset.qc <- computeSumFactors(sceset.qc, clusters = qclust)
+#Can return warning, might want to suppress if possible
+sceset.qc <- normalize(sceset.qc)
+
+# Compare raw (log normalized) with SCRAN using a relative log expression plot
+
+plotRLE(sceset[endog_genes, ], exprs_mats = list(Raw = sceset@assays$data$logcounts_raw, scran = sceset@assays$data$logcounts), exprs_logged = c(TRUE, TRUE))
+
+
+# Close PDF
+dev.off()
--- pipeline.py
+++ pipeline.py
@@ -4,20 +4,41 @@ import os
 import subprocess
 
 
-inputdirectory = "/media/imgorter/BD_1T/Iris/Data/BE_10X_pilot_II/"
+#inputdirectory = ""
+
 #if run_id is empty, use the name of the folder where the data is in
-#run_id = "10X_pilot_2"
-#output = "/media/imgorter/BD_1T/Iris/Human_Sample/outs/filtered_gene_bc_matrices/GRCh38/"
+#run_id = ""
+
+#will be auto-generated when knowing the input directory. (is output of CellRanger)
+
+#output = ""
+#generate by mm10 or GRCh38
+#species = ""
+
+
+#Annotations can be empty.
+annotations = ""
+
+
+inputdirectory = "/media/mdubbelaar/BD_1T/Iris/Data/BE_10X_pilot_II/"
+run_id = "10X_pilot_II"
+output = "/media/mdubbelaar/BD_1T/Iris/Human_Sample/outs/filtered_gene_bc_matrices/GRCh38/"
+species = "Human"
+
+#run_id = "Tung data"
+#output = "/media/mdubbelaar/BD_1T/Iris/Tung_data/"
 #species = "Human"
+#annotations = "annotations.txt"
 
-run_id = "10X_pilot_1"
-output = "/media/imgorter/BD_1T/Iris/Mouse_Sample/outs/filtered_gene_bc_matrices/mm10/"
-species = "Mouse"
 
+
+
+#user kiezen of ze willen filteren of niet
 
 
 def perform_Analysis(output, run_id):
-	subprocess.call(["Rscript", "--vanilla", "/media/imgorter/BD_1T/Iris/Scripts/Pipeline/basic_analysis.R", output, run_id, species])
+	#subprocess.call(["Rscript", "--vanilla", "/media/imgorter/BD_1T/Iris/Scripts/Pipeline/basic_analysis.R", output, run_id, species, annotations])
+	subprocess.call(["Rscript", "--vanilla", "/media/mdubbelaar/BD_1T/Iris/Scripts/Pipeline/basic_analysis.R", output, run_id, species, annotations])
 	
 	
 
